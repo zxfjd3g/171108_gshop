25fc0cb80b55bcfaff2287abae9dedaee5ef6051
zxfjd3g <258147149@qq.com>
2018-04-02 10:06:23 +0800

    搭建项目路由

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+/*
+路由器模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Msite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+
+Vue.use(VueRouter)
+
+// 向外默认暴露一个路由器对象
+export default new VueRouter({ // 配置对象
+  routes: [
+    {
+      path: '/msite',
+      component: Msite
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+
+  ]
+})
+
