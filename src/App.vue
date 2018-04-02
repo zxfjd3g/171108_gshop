ec455ee6de198d307612a7ea966d3cde69b089ca
zxfjd3g <258147149@qq.com>
2018-04-02 20:02:01 +0800

    ajax

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,14 @@
 
 <script>
 import FootGuide from './components/FootGuide/FootGuide.vue'
-
+import {reqAddress} from './api'
 export default {
+
+  async mounted () {
+    const result = await reqAddress('40.10038,116.37867')
+    console.log(result)
+  },
+
   components: {
     FootGuide
   }
